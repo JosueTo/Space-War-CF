1e6a83a66e198d92adcad4befd8ca9133468e480
Josue Torres <josuetolvera@gmail.com>
2022-05-31 21:41:34 -0500

    Se configuran spawn y movimiento de enemigos. También se actualiza la carpeta Assets

--- constants.py
+++ constants.py
@@ -1,10 +1,22 @@
 import pygame, sys, os, random, time
 
-
+# CONSTANTES DE PANTALLA
 screen_width = 800
 screen_height = 800
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Space War')
 clock = pygame.time.Clock()
+
+# IMAGENES DEL JUEGO
 BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('Assets/background/bg42.png')),(screen_width,screen_height))
-PLAYER_SHIP = pygame.transform.scale(pygame.image.load(os.path.join('Assets/Player/ship_player.png')),(screen_width,screen_height))
+# PLAYER
+PLAYER_SHIP = pygame.image.load(os.path.join('Assets/Player/ship_player.png'))
+# ENEMIES
+YELLOW_SHIP = pygame.transform.rotozoom(pygame.image.load(os.path.join('Assets/enemies/yellow-ship.png')),180,1)
+RED_SHIP = pygame.transform.rotozoom(pygame.image.load(os.path.join('Assets/enemies/red-ship.png')),180,1)
+PURPLE_SHIP = pygame.transform.rotozoom(pygame.image.load(os.path.join('Assets/enemies/purple-ship.png')),180,1)
+# BULLET / LASER
+GREEN_BULLET = pygame.image.load(os.path.join('Assets/bullets/green-bullet.png'))
+YELLOW_LASER = pygame.image.load(os.path.join('Assets/bullets/yellow-laser.png'))
+RED_LASER = pygame.image.load(os.path.join('Assets/bullets/red-laser.png'))
+PURPLE_LASER = pygame.image.load(os.path.join('Assets/bullets/purple-laser.png'))
--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 import pygame, sys, os, random, time
 from constants import *
 
-class Game:
-    def __init__(self):
-        pass
-    def screeen_draw(self):
-        # CONFIGURACIÓN DE BACKGROUND
-        # self.background = pygame.transform.scale(pygame.image.load(os.path.join('Assets/background/bg42.png')),(screen_width,screen_height))
-        screen.blit(BACKGROUND,(0,0))
-        # IMPRIMIENDO EL NIVEL Y VIDAS
-        self.font = pygame.font.SysFont('comicsans', 30)
-        self.lives = 5
-        self.lives_label = self.font.render(f'Lives: {self.lives}', 1, (255,255,255))
-        self.level = 1
-        self.level_label = self.font.render(f'Level: {self.level}', 1,(255,255,255))
-        screen.blit(self.lives_label, (10,10))
-        screen.blit(self.level_label, (screen_width - self.level_label.get_width() - 10, 10))
-    
-    def run(self):
-        self.screeen_draw()
-        player.draw(screen)
-
 class Ship:
     def __init__(self,x,y,health=100):
         self.x = x
@@ -34,42 +14,97 @@ class Ship:
     def draw(self, window):
         window.blit(self.ship_img,(self.x,self.y))
 
-class Player(Ship):
-    def __init__(self,x,y,healt=100):
-        super().__init__(x,y,healt)
-        self.ship_img = PLAYER_SHIP
+    def get_width(self):
+        return self.ship_img.get_width()
+    
+    def get_height(self):
+        return self.ship_img.get_height()
 
+class Player(Ship):
+    def __init__(self,x,y,health=100):
+        super().__init__(x,y,health)
+        self.ship_img = PLAYER_SHIP
+        self.laser_img = GREEN_BULLET
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
+
+class Enemy(Ship):
+    COLOR_MAP = {
+                "yellow": (YELLOW_SHIP,YELLOW_LASER),
+                "red": (RED_SHIP,RED_LASER),
+                "purple": (PURPLE_SHIP,PURPLE_LASER)
+                }
+
+    def __init__(self,x,y,color,health=100):
+        super().__init__(x,y,health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
+    
+    
 
 pygame.init()
 
-# CONSTANTES DE MI VENTANA
-# screen_width = 800
-# screen_height = 800
-# screen = pygame.display.set_mode((screen_width,screen_height))
-# pygame.display.set_caption('Space War')
-# clock = pygame.time.Clock()
-game = Game()
-player = Player(300,650)
-player_vel = 10
 # LOOP PRINCIPAL
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    # MOVIMIENTO DEL PLAYER
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player.x - player_vel > 0:
-        player.x -= player_vel
-    if keys[pygame.K_RIGHT] and player.x + player_vel + 50 < screen_width:
-        player.x += player_vel
-    if keys[pygame.K_UP] and player.y - player_vel > 0:
-        player.y -= player_vel
-    if keys[pygame.K_DOWN] and player.y + player_vel + 50 < screen_height:
-        player.y += player_vel
+def main():
+    level = 0
+    lives = 5
+    main_font = pygame.font.SysFont('comicsans', 30)
 
-    game.run()
+    player = Player(300,650)
+    player_vel = 10
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
+
+    def redraw_window():
+        # CONFIGURACIÓN DE BACKGROUND
+        screen.blit(BACKGROUND,(0,0))
+        # IMPRIMIENDO EL NIVEL Y VIDAS
+        lives_label = main_font.render(f'Lives: {lives}', 1, (255,255,255))
+        level_label = main_font.render(f'Level: {level}', 1,(255,255,255))
+        screen.blit(lives_label, (10,10))
+        screen.blit(level_label, (screen_width - level_label.get_width() - 10, 10))
+        # IMPRIMIENDO ENEMIGOS
+        for enemy in enemies:
+            enemy.draw(screen)
+        # IMPRIMIENDO PLAYER
+        player.draw(screen)
+
+    while True:
         
-    pygame.display.update()
-    clock.tick(60)
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, screen_width-100), random.randrange(-1500,100), random.choice(["yellow","red","purple"]))
+                enemies.append(enemy)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        # MOVIMIENTO DEL PLAYER
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - player_vel > 0:
+            player.x -= player_vel
+        if keys[pygame.K_RIGHT] and player.x + player_vel + player.get_width() < screen_width:
+            player.x += player_vel
+        if keys[pygame.K_UP] and player.y - player_vel > 0:
+            player.y -= player_vel
+        if keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() < screen_height:
+            player.y += player_vel
+        
+        # MOVIMIENTO DE ENEMIGOS
+        for enemy in enemies:
+            enemy.move(enemy_vel)
+        
+        redraw_window()    
+        pygame.display.update()
+        clock.tick(60)
+
+main()
